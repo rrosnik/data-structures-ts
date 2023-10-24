c8396a9909e8b8c3a34a71c5497f8931ef18b7f0
RezaNRC <Reza.Rostaminikoo@nrc-cnrc.gc.ca>
2023-10-24 11:54:27 -0400

    update xml.ts

--- src/tree/xml/Xml.ts
+++ src/tree/xml/Xml.ts
@@ -1,6 +1,36 @@
-import { Tree } from "../Tree";
+import { ITree } from "../ITree";
+import { XmlNode } from "./XmlNode";
 
-export  class Xml extends Tree<any> {
+export class Xml implements ITree<Element> {
+
+
+    private _root: XmlNode
+
+    get root(): XmlNode { return this._root }
+    set root(value: XmlNode) { this._root = this.root }
+
+    setRoot(value: XmlNode): this {
+        this.root = value
+        return this
+    }
+
+    traverseBF(callback: (node: XmlNode) => void) {
+        const arr: Array<XmlNode> = [this._root]
+        while (arr.length) {
+            const node: XmlNode = arr.shift() as XmlNode
+            arr.push(...node.children)
+            callback(node)
+        }
+    }
+
+    traverseDF(callback: (node: XmlNode) => void) {
+        const arr = [this._root]
+        while (arr.length) {
+            const node: XmlNode = arr.shift() as XmlNode
+            arr.unshift(...node.children)
+            callback(node)
+        }
+    }
 
 
 
